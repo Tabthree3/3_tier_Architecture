a7f40cb0866e84b88d09ec8946935335c17e9c78
Tabthree3 <142275620+Tabthree3@users.noreply.github.com>
2024-06-15 09:34:28 -0400

    updated

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -1,16 +1,16 @@
 # Internet-facing Load Balancer
 resource "aws_lb" "web_lb" {
-  name             = "web-lb"
-  security_groups  = [var.frontend_lb_sg]
-  subnets          = var.public_subnets
-  idle_timeout     = 300
+  name            = "web-lb"
+  security_groups = [module.network.frontend_lb_sg]
+  subnets         = module.network.public_subnets
+  idle_timeout    = 300
 }
 
 resource "aws_lb_target_group" "web_tg" {
   name     = "web-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = module.network.vpc_id
 
   health_check {
     path                = "/"
@@ -38,17 +38,17 @@ resource "aws_lb_listener" "web_listener" {
 
 # Internal-facing Load Balancer
 resource "aws_lb" "app_lb" {
-  name             = "app-lb"
-  subnets          = var.private_subnets
-  security_groups  = [var.app_lb_sg]
-  idle_timeout     = 300
+  name            = "app-lb"
+  subnets         = module.network.private_subnets
+  security_groups = [module.network.app_lb_sg]
+  idle_timeout    = 300
 }
 
 resource "aws_lb_target_group" "app_tg" {
   name     = "app-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  vpc_id   = module.network.vpc_id
 
   health_check {
     path                = "/"
