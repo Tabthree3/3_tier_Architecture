8600d8858ca31884425d4bef27cf1909c80d73b7
Tabthree3 <142275620+Tabthree3@users.noreply.github.com>
2024-06-15 09:21:25 -0400

    update loadbalancer

--- modules/loadbalancer/variable.tf
+++ modules/loadbalancer/variable.tf
@@ -9,3 +9,9 @@ variable "vpc_id" {}
 variable "app_lb_sg" {}
 
 variable "myazs" {}
+variable "app_sg" {
+  description = "Security Group for the application servers"
+  type        = any
+  # You can add other constraints if needed, e.g., default value
+}
+
